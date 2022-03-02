a249a1da73b718e130704866e8c3dfe2bda869bc
Brendo Silva <breendo.01@gmail.com>
2022-03-02 09:40:03 -0300

    iniciando projeto, entitie, dto

---
+++ src/main/java/com/desafio/bootcamp/clients/entities/Clients.java
@@ -0,0 +1,56 @@
+package com.desafio.bootcamp.clients.entities;
+
+import java.time.Instant;
+
+public class Clients {
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private Instant birthDate;
+    private Integer children;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
